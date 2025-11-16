57cb8d69b222e0214c76cdc686260db9417ec17d
Aariz Siddiqui <arizsiddiqui93@email.com>
2025-11-17 01:44:06 +0530

    adding updated login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ const handleSubmit = async (e)=>{
   try{
     e.preventDefault();
     console.log(user);
-    const response = await fetch('http://localhost:8000/api/auth/login',{
+    const response = await fetch('https://vercel-backend-eight-chi.vercel.app/api/auth/login',{
       method:"POST",
       headers:{
         "Content-Type":"application/json"
